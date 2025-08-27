44cd4a8219df4b5247638650e90d0e313d95c9ca
migdev78 <mdevos.mld@gmail.com>
2025-08-27 21:32:23 +0200

    amélioration du bouton connexion

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,21 +11,37 @@
 <script>
 export default {
   name: 'NavBar',
+  data() {
+    return {
+      // ⭐ Ajoute une variable de réactivité pour forcer la mise à jour
+      authStatus: !!localStorage.getItem('jwtToken')
+    };
+  },
   computed: {
     isLoggedIn() {
-      // ⭐ Check for the existence of the JWT token
-      return !!localStorage.getItem('jwtToken');
+      // ⭐ Dépend maintenant de la variable réactive
+      return this.authStatus;
     }
   },
   methods: {
     goToLogin() {
-      // ⭐ Redirect to the login page
       this.$router.push('/login');
     },
     logout() {
-      // ⭐ Clear the JWT token and redirect to login
       localStorage.removeItem('jwtToken');
+      // ⭐ Met à jour la variable réactive lors de la déconnexion
+      this.authStatus = false;
       this.$router.push('/login');
+    }
+  },
+  // ⭐ Crée un "watcher" pour écouter les changements de route
+  watch: {
+    '$route.path': function(newPath, oldPath) {
+      // Si la route change vers un endroit qui implique une connexion (par exemple, de /login à /),
+      // vérifie l'état d'authentification et met à jour
+      if (oldPath === '/login' && newPath !== '/login') {
+        this.authStatus = !!localStorage.getItem('jwtToken');
+      }
     }
   }
 };
